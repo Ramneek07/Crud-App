1304167bd9c59629e43676e6ade5a3c6c720123b
Ramneek07 <114234531+Ramneek07@users.noreply.github.com>
2025-05-19 17:33:51 +0530

    Add files via upload

---
+++ src/EmployeeData.js
@@ -0,0 +1,22 @@
+export const EmployeeData =[
+    
+    {
+        id:1,
+        firstName:'Cristiano',
+        lastName:'Ronaldo',
+        age:40
+    },
+    {
+        id:2,
+        firstName:'Virat',
+        lastName:'Kohli',
+        age:36
+    },
+    {
+        id:3,
+        firstName:'Ramneeek',
+        lastName:'Sharma',
+        age:24
+    }, 
+
+]
